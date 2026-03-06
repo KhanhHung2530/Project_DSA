26808b29dc4f8d46220af80c460c1c9f937d495c
hoccode1ngay <binhnguyenag07@gmail.com>
2026-03-06 10:24:01 +0700

    Update CalendarExport.h
    clean code

--- CalendarExport.h
+++ CalendarExport.h
@@ -9,51 +9,51 @@ using namespace std;
 
 string getStartDate(int day)
 {
-    int startDay = 23;
-    int actualDay = startDay + (day - 2);
+    int startDay = 12;
+    int actualDay = startDay + (day - 2);    // Convert weekday to actual calendar date starting from Jan 12
     string dayStr = to_string(actualDay);
     if (actualDay < 10)
         dayStr = "0" + dayStr;
-    return "202602" + dayStr;
+    return "202601" + dayStr;
 }
 
-string formatTime(string t)
+string formatTime(const string &t)
 {
     string hh = t.substr(0, 2);
     string mm = t.substr(3, 2);
     return hh + mm + "00";
 }
 
-void exportToICS(const vector<Course> &selectedCourses, string filename)
+void exportToICS(const vector<Course> &selectedCourses, const string &filename)
 {
     ofstream outfile(filename);
     if (!outfile)
     {
-        cout << "Error: Could not create file " << filename << endl;
+        cout << "Error: Could not create file " << filename << '\n';
         return;
     }
 
-    outfile << "BEGIN:VCALENDAR" << endl;
-    outfile << "VERSION:2.0" << endl;
+    outfile << "BEGIN:VCALENDAR\n";
+    outfile << "VERSION:2.0\n";
 
     for (const Course &c : selectedCourses)
     {
-        outfile << "BEGIN:VEVENT" << endl;
-        outfile << "SUMMARY:" << c.course_id << endl;
-        outfile << "LOCATION:" << c.room << endl;
+        outfile << "BEGIN:VEVENT\n";
+        outfile << "SUMMARY:" << c.course_id << '\n';
+        outfile << "LOCATION:" << c.room << '\n';
 
-        string dateStr = getStartDate(c.day);
-        string startTime = formatTime(c.start_time);
-        string endTime = formatTime(c.end_time);
+        const string dateStr = getStartDate(c.day);
+        const string startTime = formatTime(c.start_time);
+        const string endTime = formatTime(c.end_time);
 
-        outfile << "DTSTART:" << dateStr << "T" << startTime << endl;
-        outfile << "DTEND:" << dateStr << "T" << endTime << endl;
-        outfile << "RRULE:FREQ=WEEKLY;COUNT=15" << endl;
+        outfile << "DTSTART:" << dateStr << "T" << startTime << '\n';
+        outfile << "DTEND:" << dateStr << "T" << endTime << '\n';
+        outfile << "RRULE:FREQ=WEEKLY;COUNT=15\n";
 
-        outfile << "END:VEVENT" << endl;
+        outfile << "END:VEVENT\n";
     }
 
-    outfile << "END:VCALENDAR" << endl;
+    outfile << "END:VCALENDAR\n";
     outfile.close();
 }
 
